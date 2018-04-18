1d6d9bbc1d68290ef7c74bd9b6cebceb77bad2d6
Joe McKinney and Pablo Barreyro <pablo@>
2018-04-18 10:57:37 -0700

    add a second passing spec to the spec file

--- spec/haiku_spec.js
+++ spec/haiku_spec.js
@@ -1,10 +1,14 @@
 import { checkLines } from './../src/project.js';
-
+import { splitSentences } from './../src/project.js';
 describe('Haiku', function() {
   let reusableText = "Haikus are easy.\nBut sometimes they don't make sense.\nRefrigerator.";
 
   it('should test whether a poem has three lines', function() {
     expect(checkLines(reusableText)).toEqual(2);
+  });
+
+  it('should split text into sentences at a new line break', function() {
+    expect(splitSentences(reusableText).length).toEqual(3);
   });
 
 
