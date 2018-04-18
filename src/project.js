4aa5e854bb57633614a664a731c23dd49bcf8ef0
Joe McKinney and Pablo Barreyro <pablo@>
2018-04-18 11:17:48 -0700

    add passing function to count vowels in every sentence of input text

--- src/project.js
+++ src/project.js
@@ -2,3 +2,18 @@ export function checkLines(text) {
   let newlineCount = text.match(/.\n./g).length;
   return newlineCount;
 }
+
+export function splitSentences(text) {
+  let sentenceArray = text.split(/\n/g);
+  return sentenceArray;
+}
+
+export function countVowels(text) {
+  let sentenceArray = splitSentences(text);
+  let countArray = [];
+
+  for (let i = 0; i < sentenceArray.length; i++) {
+    countArray.push(sentenceArray[i].match(/[aeiou]/gi).length);
+  }
+  return countArray;
+}
