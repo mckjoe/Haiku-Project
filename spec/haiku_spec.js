225b34bebabf0f46d300ffad3b8e9fde3ad1e233
Joe McKinney, Pablo Barreyro and Sean McDermott <joe@>
2018-04-19 15:25:38 -0700

    add final test with failing result

--- spec/haiku_spec.js
+++ spec/haiku_spec.js
@@ -1,8 +1,5 @@
 import { checkLines, validateText } from './../src/validate.js';
 import { Haiku } from './../src/haiku.js';
-// import { countWordsEndingE } from './../src/project.js';
-// import { updateVowelCount } from './../src/project.js';
-
 
 describe('Haiku', function() {
   let reusableText = "Haikus are easy.\nBut sumtimes they don't make sense.\nRefrigerator.";
@@ -10,6 +7,8 @@ describe('Haiku', function() {
 
   beforeEach(function() {
     testHaiku = new Haiku(reusableText);
+    testHaiku.countVowels();
+
   });
 
   it('should check text for unwanted charactors', function() {
@@ -25,17 +24,16 @@ describe('Haiku', function() {
   });
 
   it('should count the number of vowels, vowel pairs, and qualifying ys in each sentence', function() {
-    testHaiku.countVowels();
     expect(testHaiku.vowelCounts).toEqual([6,10,5]);
   });
 
-  // it('should count the number of instances that a word ends with ie, e, but not le, in each sentence', function() {
-  //   expect(countWordsEndingE(reusableText)).toEqual([1,2,0]);
-  // });
-  //
-  // it('should subtract number of words that end in e from the number of vowels in the sentence', function() {
-  //   expect(updateVowelCount(reusableText)).toEqual([5,9,5]);
-  // });
+  it('should count the number of instances that a word ends with ie, e, but not le, in each sentence', function() {
+    testHaiku.filterExclusions();
+    expect(testHaiku.vowelCounts).toEqual([5,7,5]);
+  });
 
+  it('should determine if a poem is a Haiku', function() {
+    expect(testHaiku.pass).toEqual(true);
+  });
 
 });
