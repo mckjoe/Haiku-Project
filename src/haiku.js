ee3baec36386ce14549d44bed2da4b663ea2b701
Joe McKinney, Pablo Barreyro and Sean McDermott <sean@>
2018-04-19 15:17:35 -0700

    remove the this.exclusions as property of Haiku

--- src/haiku.js
+++ src/haiku.js
@@ -3,11 +3,24 @@ class Haiku {
     this.lines = text.split(/\n/g);
     this.vowelCounts = [];
     this.pass = false;
+    this.exclusions = [];
   }
 
   countVowels() {
     for (let i = 0; i < this.lines.length; i++) {
       this.vowelCounts.push(this.lines[i].match(/[aeu]+|i|[cts]ia+|e[^o]\W|[ou]+/gi).length);
+    }
+  }
+
+  filterExclusions() {
+    let exclusionCounts = [];
+    for (var i = 0; i < this.lines.length; i++) {
+      if (this.lines[i].match(/[a-z]+e\s/gi) !== null) {
+        exclusionCounts.push(this.lines[i].match(/ie|[a-z]+[^lh]e[sd]*\W|\s[^rh]ele/gi).length);
+      } else {
+        exclusionCounts.push(0);
+      }
+      this.vowelCounts[i] = this.vowelCounts[i] - exclusionCounts[i];
     }
   }
 
