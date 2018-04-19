f4af0469284b19cd12325152451cafbc5d9b784d
Joe McKinney, Pablo Barreyro and Sean McDermott <joe@>
2018-04-19 13:47:29 -0700

    update checkVowels spec to pass with updated test string

--- spec/haiku_spec.js
+++ spec/haiku_spec.js
@@ -1,11 +1,16 @@
-// import { checkLines, validateText } from './../src/validate.js';
-// import { Haiku } from './../src/haiku.js';
+import { checkLines, validateText } from './../src/validate.js';
+import { Haiku } from './../src/haiku.js';
 // import { countWordsEndingE } from './../src/project.js';
 // import { updateVowelCount } from './../src/project.js';
 
 
 describe('Haiku', function() {
   let reusableText = "Haikus are easy.\nBut sumtimes they don't make sense.\nRefrigerator.";
+  let testHaiku;
+
+  beforeEach(function() {
+    testHaiku = new Haiku(reusableText);
+  });
 
   it('should check text for unwanted charactors', function() {
     expect(validateText(reusableText)).toEqual(false);
@@ -16,14 +21,12 @@ describe('Haiku', function() {
   });
 
   it('should split text into sentences at a new line break', function() {
-    testHaiku = new Haiku(reusableText);
     expect(testHaiku.lines.length).toEqual(3);
   });
 
   it('should count the number of vowels, vowel pairs, and qualifying ys in each sentence', function() {
-    testHaiku = new Haiku(reusableText);
     testHaiku.countVowels();
-    expect(testHaiku.vowelCounts).toEqual([6,11,5]);
+    expect(testHaiku.vowelCounts).toEqual([6,10,5]);
   });
 
   // it('should count the number of instances that a word ends with ie, e, but not le, in each sentence', function() {
